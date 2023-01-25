308a9951ec5aa3bdd60c87259a771633eda41498
Aleksandr Zubov <alexanderzubov@icloud.com>
2023-01-25 03:31:54 +0100

    saving NSObject

---
+++ Project12b_100days/Project10_100days/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10_100days
+//
+//  Created by user228564 on 1/20/23.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project12b_100days/Project10_100days/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10_100days
+//
+//  Created by user228564 on 1/19/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
